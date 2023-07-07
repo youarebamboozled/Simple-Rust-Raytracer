2283bcfaca610afbfd464b10aea3d179413b38ac
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-07-07 14:13:09 +0200

    Update object.rs

--- src/structs/object.rs
+++ src/structs/object.rs
@@ -2,7 +2,7 @@ use crate::structs::intersection::Intersection;
 use crate::structs::material::Material;
 use crate::structs::ray::Ray;
 
-pub trait Object {
+pub(crate) trait Object {
     fn intersect(&self, ray: &Ray) -> Option<Intersection>;
     fn material(&self) -> &Material;
 }
