7a8b725ab43680fdaf08b89e41cc8ef4813766b6
youarebamboozled <56235097+youarebamboozled@users.noreply.github.com>
2023-07-08 11:32:49 +0200

    Update light.rs

--- src/structs/light.rs
+++ src/structs/light.rs
@@ -1,7 +1,9 @@
 #![allow(dead_code)]
 use crate::structs::vector3::Vector3;
+use crate::structs::vector4::Vector4;
 
 pub(crate) struct Light {
     pub(crate) position: Vector3,
     pub(crate) intensity: f32,
+    pub(crate) color: Vector4,
 }
